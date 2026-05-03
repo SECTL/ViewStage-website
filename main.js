98e161042c0ed1d6fcd547ae656f6fb06e05d76e
ospneam <2140857671@qq.com>
2026-05-04 06:13:15 +0800

    修复脚页异常

--- main.js
+++ main.js
@@ -338,3 +338,8 @@ initTheme();
 initScrollAnimations();
 initColorPicker();
 initPenSizeSlider();
+
+const yearElement = document.getElementById('current-year');
+if (yearElement) {
+    yearElement.textContent = new Date().getFullYear();
+}
